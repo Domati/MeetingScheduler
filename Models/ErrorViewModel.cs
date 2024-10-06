d646fc53c81102b10ed936c92188c80dfdac48e3
Mateusz Domagalski <S59119@student.ubb.edu.pl>
2024-10-06 11:27:57 +0200

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeetingScheduler.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
